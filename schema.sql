caf7556d625decc6f334816ff72334c3e69f2be0
Gabrielli5047 <gabrielli.meloo16@gmail.com>
2022-07-07 16:26:40 -0300

    Funcao para realizar deposito funcionando e atualizando no banco de dados

--- schema.sql
+++ schema.sql
@@ -1,8 +1,25 @@
+
 CREATE DATABASE konv_transactions;
+drop table clientes
+drop table transactions
 
 create table clientes (
   	id serial primary key,
   	name text not null,
     password text not null,
-	cpf VARCHAR(15) unique
+	  cpf VARCHAR(15) unique not null,
+  	saldo integer DEFAULT 0 not null
   );
+  
+create table transactions (
+  	id serial primary key,
+    cliente_id BIGINT UNSIGNED not null,
+  	date_transaction date not null,
+    type_transaction text not null,
+    CONSTRAINT FK_cliente_id FOREIGN KEY (cliente_id)REFERENCES clientes(id)
+ );  
+
+delete from clientes where id =5 ;
+
+
+
