b407216727e6b35f12ad78d657caf12b6c285916
Gabrielli5047 <gabrielli.meloo16@gmail.com>
2022-07-07 13:03:07 -0300

    testando configuracoes bases

--- src/routes.js
+++ src/routes.js
@@ -2,5 +2,8 @@ const express = require('express');
 
 const routes = express();
 
+routes.get('/servidor', (req, res)=>{
+    res.status(200).json({message: "Servidor ok!!"})
+})
 
 module.exports = routes
