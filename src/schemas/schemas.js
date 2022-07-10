e4cfbc33b49f53851d58a30d1af8c7ce269b3bb9
Gabrielli5047 <gabrielli.meloo16@gmail.com>
2022-07-09 23:25:05 -0300

    Atualizando schema de validacao de realizar deposito

--- src/schemas/schemas.js
+++ src/schemas/schemas.js
@@ -1,17 +1,19 @@
-const Yup = require('yup');
+const Yup = require("yup");
+const isValidCPF = require("../utills/isValidCPF");
 
-const makeDepositSchema = Yup.object({
-  cpf: Yup.string('Deve ser uma string')
-          .min(11, 'Deve ter no mínimo 11 caracteres')
-          .max(14, 'Deve ter no máximop 14 caracteres')
-          .required('É necessário informar o cpf'),
-  value: Yup.number('Deve ser um número')
-            .min(1, 'Deve ser no mínimo 1')
-            .required('É necessário informar o valor'),
-  description: Yup.string('Deve ser uma string')
-                  .required('É necessário informar uma descrição'),
+exports.makeDepositSchema = Yup.object({
+  cpf: Yup.string("Deve ser uma string")
+    .min(11, "Deve ter no mínimo 11 caracteres")
+    .max(14, "Deve ter no máximop 14 caracteres")
+    .required("É necessário informar o cpf")
+    .test({
+      message: "Cpf inválido",
+      test: (value) => isValidCPF(value),
+    }),
+  value: Yup.number("Deve ser um número")
+    .min(1, "Deve ser no mínimo 1")
+    .required("É necessário informar o valor"),
+  description: Yup.string("Deve ser uma string").required(
+    "É necessário informar uma descrição"
+  ),
 });
-
-module.exports = {
-  makeDepositSchema
-}
