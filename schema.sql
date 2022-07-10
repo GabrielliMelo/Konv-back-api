b6ec237bd5c21a60a066520842cb4b195787106b
Gabrielli5047 <gabrielli.meloo16@gmail.com>
2022-07-10 19:04:55 -0300

    Criado rota de transferencia de cpf para cpf - seguindo padrao de camada de serviços / arquitetura

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-create table clientes (
+create table clients (
       id serial primary key,
       name text not null,
       cpf VARCHAR(15) unique not null,
@@ -12,7 +12,7 @@ create table transactions (
   	hour text not null,
   	description text not null,
     type_transaction text not null,
-	option_transaction integer DEFAULT '0' not null,
+	  option_transaction integer DEFAULT '0' not null,
   	value_transaction BIGINT DEFAULT 0 not null,
   	cpf_transfer varchar(15),
     CONSTRAINT FK_cliente_id FOREIGN KEY (cliente_id)REFERENCES clientes(id)
